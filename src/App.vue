d7b376f62dcd82422480ddfd4bd5048a88da5527
okyungjin <okyungjin@gmail.com>
2022-04-21 18:00:39 +0900

    TodoInput 컴포넌트 등록

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <div id="app">Hi</div>
+  <div>
+    <h1>Vue Todo with TypeScript</h1>
+    <TodoInput></TodoInput>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import TodoInput from './components/TodoInput.vue';
 
 export default Vue.extend({
-  name: 'App',
+  components: { TodoInput },
+  data() {
+    return {
+      todoText: '',
+    };
+  },
 });
 </script>
+
+<style scoped></style>
