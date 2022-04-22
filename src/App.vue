3fe9429374eb7b7cd5b2cccac3ca26c01b1a1b0d
okyungjin <okyungjin@gmail.com>
2022-04-22 16:02:01 +0900

    fetchTodoItems title 기준 오름차순 정렬

--- src/App.vue
+++ src/App.vue
@@ -67,7 +67,11 @@ export default Vue.extend({
       this.fetchTodoItems();
     },
     fetchTodoItems(): void {
-      this.todoItems = storage.fetch();
+      this.todoItems = storage.fetch().sort((a, b) => {
+        if (a.title < b.title) return -1;
+        if (a.title > b.title) return 1;
+        return 0;
+      });
     },
     initTodoText(): void {
       this.todoText = '';
