7e64fa352e07d566450c871105402f1c147e78a6
okyungjin <okyungjin@gmail.com>
2022-04-22 14:21:17 +0900

    TodoListItem 추가

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    items: {
+      type: Array,
+      //   default: [],
+    },
+  },
+});
+</script>
+
+<style scoped></style>
