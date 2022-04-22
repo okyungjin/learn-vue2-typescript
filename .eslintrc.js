38e9914b4a5641a14dad93f42b6dc07bb72e0f4a
okyungjin <okyungjin@gmail.com>
2022-04-22 14:18:06 +0900

    [ESLint] sort-keys rule 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,22 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript/recommended', '@vue/prettier'],
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+  ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'sort-keys': [
+      'error',
+      'asc',
+      { caseSensitive: true, natural: false, minKeys: 2 },
+    ],
   },
 };
